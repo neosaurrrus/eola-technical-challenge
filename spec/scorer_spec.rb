e75a648b192c74d3e4b054c6eb7ec151f53be66f
Lukie Kang <lukiek@gmail.com>
2021-02-10 15:32:25 +0000

    test: add calculate_score_from_file test

--- spec/scorer_spec.rb
+++ spec/scorer_spec.rb
@@ -16,21 +16,20 @@ RSpec.describe "Scorer" do
         end
     end
 
-    # describe ".sorter" do
-    #     it "is an instance method that sorts the provided array into alphabetical order" do
-    #         expect (:scorer.sorter['TOM','DIANA','CHARLIE']).to eq(['CHARLIE','DIANA','TOM'])
-    #     end
-    # end
-
     describe ".file_to_array" do
       it "is an instance method that opens the file referenced in path and returns a sorted array" do
         expect(scorer.file_to_array).to eq(["BARBARA", "ELIZABETH", "LINDA", "MARY", "PATRICIA"])
       end
     end
 
-    describe ".calculate_score" do
-      it "is an instance method that takes names array and calculates the correct score" do
-        expect(scorer.calculate_score).to eq(30)
+    describe ".calculate_score_from_array" do
+      it "is an instance method that takes a names array and calculates the correct score" do
+        expect(scorer.calculate_score_from_array).to eq(952)
+      end
+    end
+    describe ".calculate_score_from_file" do
+      it "is an instance method that uses file_to_array and calculate_score_from_array to get the correct score from the file" do
+        expect(scorer.calculate_score_from_file).to eq(952)
       end
     end
 
