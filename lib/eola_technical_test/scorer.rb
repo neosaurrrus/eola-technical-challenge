e97452d05a33d00bd53ac849e0909e79f351b745
Lukie Kang <lukiek@gmail.com>
2021-02-10 15:33:37 +0000

    add: calculate_score_from_file

--- lib/eola_technical_test/scorer.rb
+++ lib/eola_technical_test/scorer.rb
@@ -9,21 +9,24 @@ class Scorer
     end
 
     def file_to_array 
-    # reads file and returns a sorted array of names
-    names = CSV.read(@path, "r").flat_map{ |e| e }
-    @names = names.sort
+      # reads file and returns a sorted array of names
+      names = CSV.read(@path, "r").flat_map{ |e| e }
+      @names = names.sort
     end
 
-    def calculate_score
+    def calculate_score_from_array
       total_score = 0
       @names.each_with_index {|name, index| 
         word_score = 0
         chars = name.split('')
-        chars.each { |c| word_score += c.ord-64}
-        puts word_score
-        total_score += (word_score * (index+1))
+          chars.each { |c| word_score += c.ord-64}
+          total_score += (word_score * (index+1))
       }
       total_score
     end
 
+    def calculate_score_from_file
+      self.file_to_array
+      self.calculate_score_from_array
+    end
 end
