ef7d57412e806143d9c3184676b00c55928ec3ad
Lukie Kang <lukiek@gmail.com>
2021-02-10 11:54:55 +0000

    test: add before book for path argument

--- spec/scorer_spec.rb
+++ spec/scorer_spec.rb
@@ -1,20 +1,29 @@
 
 RSpec.describe "Scorer" do
 
-    let (:scorer) {Scorer.new}
-
+    before do
+        @scorer = Scorer.new("names.txt")
+    end
+    
     describe ".initialize" do
         it "is an initialization method that puts out a greeting to the user" do
-          expect do Scorer.new
+          expect do Scorer.new("names")
           end.to output("Welcome to Name Scorer\n").to_stdout
         end
+
+        # it "is an initialization method that contains a file path" do
+        #   expect (scorer).to 
+     
+        # end
     end
 
-    describe ".sorter" do
-        it "is an instance method that sorts the provided array into alphabetical order" do
-            expect (:scorer.sorter['TOM','DIANA','CHARLIE']).to eq(['CHARLIE','DIANA','TOM'])
-        end
-    end
+
+
+    # describe ".sorter" do
+    #     it "is an instance method that sorts the provided array into alphabetical order" do
+    #         expect (:scorer.sorter['TOM','DIANA','CHARLIE']).to eq(['CHARLIE','DIANA','TOM'])
+    #     end
+    # end
 
 
 
