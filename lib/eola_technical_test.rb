0ef82579ff17dc978e20b1045781188df8e54667
Lukie Kang <lukiek@gmail.com>
2021-02-11 11:56:07 +0000

    refactor: move code to eola_technical_test file.

--- lib/eola_technical_test.rb
+++ lib/eola_technical_test.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 require_relative "./eola_technical_test/scorer"
-
 require_relative "eola_technical_test/version"
 
 module EolaTechnicalTest
   class Error < StandardError; end
-  # Your code goes here...
+  puts Scorer.new("names.txt").calculate_score_from_file
 end
